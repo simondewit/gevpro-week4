0b5c454c865e658b77ae01f79ef57b1f3ac4becd
Simon de Wit <simon@RN-145-97-136-255.eduroam.rug.nl>
2015-02-27 09:55:32 +0100

    Added if/else statement for length of arguments given

--- spontal_filter.py
+++ spontal_filter.py
@@ -6,18 +6,21 @@ import sys
 
 
 def main(argv):
-    inp_f = argv[1]
-    outp_f = argv[2]
-    tree = ET.parse(inp_f)
-    root = tree.getroot()
-    for point in root.findall('POINT'):
-        bottom = float(point.find('BOTTOM_HZ').text)
-        top = float(point.find('TOP_HZ').text)
-        start = float(point.find('F0_START').text)
-        end = float(point.find('F0_END').text)
-        if not bottom <= start <= top or not bottom <= end <= top:
-            root.remove(point)
-    tree.write(outp_f)
+    if len(argv) == 3:
+        inp_f = argv[1]
+        outp_f = argv[2]
+        tree = ET.parse(inp_f)
+        root = tree.getroot()
+        for point in root.findall('POINT'):
+            bottom = float(point.find('BOTTOM_HZ').text)
+            top = float(point.find('TOP_HZ').text)
+            start = float(point.find('F0_START').text)
+            end = float(point.find('F0_END').text)
+            if not bottom <= start <= top or not bottom <= end <= top:
+                root.remove(point)
+        tree.write(outp_f)
+    else:
+        print("Usage: give 2 arg, 1 input file and 1 output file")
 
 
 
